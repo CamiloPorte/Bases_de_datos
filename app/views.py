e20d2529260d416f83ccad7fb626776440773445
CamiloPorteMac <camilo.porte@mail.udp.cl>
2018-07-09 19:01:00 -0400

    Nueva versión 0.0.7
    Nueva versión 0.0.7:
    
    -Se crea la asignación de administradores.-

--- app/views.py
+++ app/views.py
@@ -14,6 +14,15 @@ def formulario():
 def login():
 
 	sql ="""
+	SELECT id_menu
+	FROM menus
+	;
+	"""
+	print sql
+	cur.execute(sql)
+	menus = cur.fetchall()
+
+	sql ="""
 	SELECT num_mesa,clientes_totales
 	FROM(select SUM(capacidad) as clientes_totales,num_mesa
 	FROM pedidos , mesas
@@ -206,7 +215,7 @@ def login():
 			 top = Top, Topventa=Topventa,Topprecio=Topprecio,tophoras=tophorascompras,
 			 tophorasing=tophorasing,topAsistencia=topAsistencia,rut= rut,numeroClientes=numeroClientes,
 			 ingresoTotal = ingresoTotal,ingresoFumadores=ingresoFumadores,ingresoNoFumadores=ingresoNoFumadores,
-			 numeroClientesfumadores=numeroClientesfumadores,numeroClientesNofumadores=numeroClientesNofumadores)
+			 numeroClientesfumadores=numeroClientesfumadores,numeroClientesNofumadores=numeroClientesNofumadores,menus = menus)
 	else:
 
 		return render_template("login.html")
@@ -214,15 +223,184 @@ def login():
 @app.route('/registrarse', methods=["POST","GET"])
 def registrarse():
 
-	sql="""SELECT nombre,apellido FROM empleados,pedidos WHERE empleado.rut = pedidos.rut_empleado AND count(cant_atendida,fecha_id,rut_empleado,num_mesa,idmenu) GROUP BY empleados.rut"""
+	sql ="""
+	SELECT id_menu
+	FROM menus
+	;
+	"""
+	print sql
+	cur.execute(sql)
+	menus = cur.fetchall()
+
+	sql ="""
+	SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	GROUP BY num_mesa, hora) as holi
+	ORDER BY (clientes_totales) desc;
+	"""
+	print sql
+	cur.execute(sql)
+	Topventa = cur.fetchall()
+
+	sql ="""
+	SELECT hora,cont
+	FROM (select COUNT( DISTINCT rut_empleado) AS cont, hora
+	FROM pedidos
+	GROUP BY hora)AS holi
+	ORDER BY(cont) DESC;
+	"""
+	print sql
+	cur.execute(sql)
+	tophorascompras = cur.fetchall()
+
+	sql ="""
+	SELECT nombre,apellido,cont
+	FROM (select count(hora) AS cont, nombre, apellido
+	FROM pedidos, empleados
+	WHERE empleados.rut = pedidos.rut_empleado
+	GROUP BY empleados.rut,fecha,hora)AS holi
+	ORDER BY(cont) DESC;
+	"""
+	print sql
+	cur.execute(sql)
+	Top = cur.fetchall()
+
+	sql ="""
+	SELECT nombre,apellido,cont
+	FROM (select count(DISTINCT fecha) AS cont, nombre, apellido
+	FROM pedidos, empleados
+	WHERE empleados.rut = pedidos.rut_empleado
+	GROUP BY empleados.rut,fecha)AS holi
+	ORDER BY(cont) DESC;
+	"""
+	print sql
+	cur.execute(sql)
+	topAsistencia = cur.fetchall()
+
+	sql ="""
+	SELECT num_mesa,valores_totales,fecha,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales,num_mesa,fecha, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY num_mesa ,fecha,hora) as holi
+	ORDER BY (valores_totales) desc;
+	"""
+	print sql
+	cur.execute(sql)
+	Topprecio= cur.fetchall()
+
+	sql ="""
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY hora) as holi
+	ORDER BY (valores_totales) desc;
+	"""
+	print sql
+	cur.execute(sql)
+	tophorasing = cur.fetchall()
 
 	sql="""SELECT * FROM empleados"""
 	cur.execute(sql)
 	empleados = cur.fetchall()
 
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientes = cur.fetchone()
+
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoTotal = cur.fetchone()
+
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus,mesas
+	WHERE pedidos.idmenu = menus.id_menu
+	AND mesas.numero = pedidos.num_mesa
+	AND mesas.zona_fumadores = 'True'
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoFumadores = cur.fetchone()
+
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus,mesas
+	WHERE pedidos.idmenu = menus.id_menu
+	AND mesas.numero = pedidos.num_mesa
+	AND mesas.zona_fumadores = 'false'
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoNoFumadores = cur.fetchone()
+
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	AND zona_fumadores = 'false'
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientesNofumadores = cur.fetchone()
+
+
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	AND zona_fumadores = 'True'
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientesfumadores = cur.fetchone()
+
+	print sql
+	cur.execute(sql)
+	Top = cur.fetchall()
+
+
 	sql="""SELECT nombre, apellido FROM empleados"""
 	cur.execute(sql)
 	nombres = cur.fetchone()
+
 	alerta=""
 	if request.method == "POST":
 		password=request.form["password"]
@@ -230,13 +408,6 @@ def registrarse():
 		nombre=request.form["nombre"]
 		apellido=request.form["apellido"]
 
-		sql="""SELECT nombre,apellido FROM empleados WHERE empleados.rut = '%s'
-		"""%(rut)
-		cur.execute(sql)
-		nombre = cur.fetchone()
-
-		print(rut)
-		print(rut)
 		if len(rut) >10 and len(rut) < 9:
 			alerta="Rut invalido vuelva ingresar"
 			return render_template("register.html", alerta = alerta)
@@ -248,7 +419,8 @@ def registrarse():
 		print(resultadoempleado)
 
 		if len(resultadoempleado) == 0 :
-			sql ="""INSERT INTO empleados (rut,nombre,apellido,password,admin) VALUES ('%s','%s','%s','%s',false);
+
+			sql ="""INSERT INTO empleados (rut,nombre,apellido,password,admin) VALUES ('%s','%s','%s','%s','false');
 			"""%(rut,nombre,apellido,password)
 			cur.execute(sql)
 
@@ -257,7 +429,17 @@ def registrarse():
 			cur.execute(sql)
 			nombre = cur.fetchone()
 			conn.commit()
-			return render_template("tables.html", empleados = empleados , administrador=admin , name = nombres,alerta = alerta,top=top)
+
+			sql="""SELECT admin FROM empleados WHERE empleados.rut = '%s'
+			"""%(rut)
+			cur.execute(sql)
+			admin = cur.fetchone()
+
+			return render_template("tables.html", empleados = empleados , administrador=admin , name = nombre,
+			 top = Top, Topventa=Topventa,Topprecio=Topprecio,tophoras=tophorascompras,
+			 tophorasing=tophorasing,topAsistencia=topAsistencia,rut= rut,numeroClientes=numeroClientes,
+			 ingresoTotal = ingresoTotal,ingresoFumadores=ingresoFumadores,ingresoNoFumadores=ingresoNoFumadores,
+			 numeroClientesfumadores=numeroClientesfumadores,numeroClientesNofumadores=numeroClientesNofumadores ,menus=menus)
 
 		elif len(resultadoempleado) != 0:
 			alerta="Rut ya utilizado"
@@ -268,41 +450,189 @@ def registrarse():
 		return render_template("register.html",alerta=alerta)
 
 
-@app.route('/tables',methods=["POST","GET"])
-def actualizacion():
+@app.route('/tables/<rut>',methods=["POST","GET"])
+def home(rut):
+
+	sql="""SELECT * FROM empleados WHERE empleados.rut ='%s';
+	"""%(rut)
+	cur.execute(sql)
+	resultadoempleado = cur.fetchall()
+	print(resultadoempleado)
+
+	sql ="""
+	SELECT id_menu FROM menus ;
+	"""
+	print sql
+	cur.execute(sql)
+	menus = cur.fetchall()
+	sql ="""
+	SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	GROUP BY num_mesa, hora) as holi
+	ORDER BY (clientes_totales) desc;
+	"""
+	print sql
+	cur.execute(sql)
+	Topventa = cur.fetchall()
+
+	sql ="""
+	SELECT hora,cont
+	FROM (select COUNT( DISTINCT rut_empleado) AS cont, hora
+	FROM pedidos
+	GROUP BY hora)AS holi
+	ORDER BY(cont) DESC;
+	"""
+	print sql
+	cur.execute(sql)
+	tophorascompras = cur.fetchall()
 
 	sql ="""
 	SELECT nombre,apellido,cont
-	FROM (select count(fecha_id) AS cont, nombre, apellido
+	FROM (select count(hora) AS cont, nombre, apellido
 	FROM pedidos, empleados
 	WHERE empleados.rut = pedidos.rut_empleado
-	GROUP BY empleados.rut)AS holi
+	GROUP BY empleados.rut,fecha,hora)AS holi
 	ORDER BY(cont) DESC;
 	"""
 	print sql
 	cur.execute(sql)
 	Top = cur.fetchall()
-	return render_template("tables.html",top = Top)
 
-@app.route('/test/<rut>', methods=["POST","GET"])
-def test(rut):
-
-	sql="""
-	SELECT admin
-	FROM empleados
-	WHERE empleados.rut = '%s'
-	"""%(rut)
+	sql ="""
+	SELECT nombre,apellido,cont
+	FROM (select count(DISTINCT fecha) AS cont, nombre, apellido
+	FROM pedidos, empleados
+	WHERE empleados.rut = pedidos.rut_empleado
+	GROUP BY empleados.rut,fecha)AS holi
+	ORDER BY(cont) DESC;
+	"""
+	print sql
 	cur.execute(sql)
-	admin = cur.fetchone()
+	topAsistencia = cur.fetchall()
 
-	sql="""SELECT nombre,apellido
-	FROM empleados
-	WHERE empleados.rut = '%s'
-	"""%(rut)
+	sql ="""
+	SELECT num_mesa,valores_totales,fecha,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales,num_mesa,fecha, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY num_mesa ,fecha,hora) as holi
+	ORDER BY (valores_totales) desc;
+	"""
+	print sql
 	cur.execute(sql)
-	nombre = cur.fetchone()
+	Topprecio= cur.fetchall()
 
-	return render_template("forms.html",administrador=admin,name = nombre)
+	sql ="""
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY hora) as holi
+	ORDER BY (valores_totales) desc;
+	"""
+	print sql
+	cur.execute(sql)
+	tophorasing = cur.fetchall()
+
+	sql="""SELECT * FROM empleados"""
+	cur.execute(sql)
+	empleados = cur.fetchall()
+
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientes = cur.fetchone()
+
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus
+	WHERE pedidos.idmenu = menus.id_menu
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoTotal = cur.fetchone()
+
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus,mesas
+	WHERE pedidos.idmenu = menus.id_menu
+	AND mesas.numero = pedidos.num_mesa
+	AND mesas.zona_fumadores = 'True'
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoFumadores = cur.fetchone()
+	sql ="""
+	SELECT SUM(valores_totales)
+	FROM (
+	SELECT valores_totales,hora
+	FROM(select SUM(cant_atendida*menus.precio_menu) as valores_totales, hora
+	FROM pedidos,menus,mesas
+	WHERE pedidos.idmenu = menus.id_menu
+	AND mesas.numero = pedidos.num_mesa
+	AND mesas.zona_fumadores = 'false'
+	GROUP BY hora) as holi)
+	AS titoFernANDes;
+	"""
+	print sql
+	cur.execute(sql)
+	ingresoNoFumadores = cur.fetchone()
+
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	AND zona_fumadores = 'false'
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientesNofumadores = cur.fetchone()
+
+
+	sql ="""
+	SELECT SUM (clientes_totales)
+	FROM ( SELECT num_mesa,clientes_totales
+	FROM(select SUM(capacidad) as clientes_totales,num_mesa
+	FROM pedidos , mesas
+	WHERE mesas.numero = pedidos.num_mesa
+	AND zona_fumadores = 'True'
+	GROUP BY num_mesa, hora) as holi)as preotriano;
+	"""
+	print sql
+	cur.execute(sql)
+	numeroClientesfumadores = cur.fetchone()
+
+	print sql
+	cur.execute(sql)
+	Top = cur.fetchall()
+	return render_template("tables.html", empleados = empleados , administrador=admin , name = nombre,
+	 top = Top, Topventa=Topventa,Topprecio=Topprecio,tophoras=tophorascompras,
+	 tophorasing=tophorasing,topAsistencia=topAsistencia,rut= rut,numeroClientes=numeroClientes,
+	 ingresoTotal = ingresoTotal,ingresoFumadores=ingresoFumadores,ingresoNoFumadores=ingresoNoFumadores,
+	 numeroClientesfumadores=numeroClientesfumadores,numeroClientesNofumadores=numeroClientesNofumadores,resultadoempleado=resultadoempleado)
+
 
 @app.route('/busqueda', methods=["POST","GET"])
 def busqueda():
@@ -338,3 +668,43 @@ def mercaderia():
 		merca = cur.fetchall()
 
 	return render_template(merca=merca)
+
+@app.route('/test/<rut>', methods=["POST","GET"])
+def test(rut):
+
+	sql="""
+	SELECT admin
+	FROM empleados
+	WHERE empleados.rut = '%s'
+	"""%(rut)
+	cur.execute(sql)
+	admin = cur.fetchone()
+
+	sql="""SELECT nombre,apellido
+	FROM empleados
+	WHERE empleados.rut = '%s'
+	"""%(rut)
+	cur.execute(sql)
+	nombre = cur.fetchone()
+
+	return render_template("megazord.html",administrador=admin,name = nombre)
+
+@app.route('/ventas/<rut>', methods=["POST","GET"])
+def ventas(rut):
+
+	sql="""
+	SELECT admin
+	FROM empleados
+	WHERE empleados.rut = '%s'
+	"""%(rut)
+	cur.execute(sql)
+	admin = cur.fetchone()
+
+	sql="""SELECT nombre,apellido
+	FROM empleados
+	WHERE empleados.rut = '%s'
+	"""%(rut)
+	cur.execute(sql)
+	nombre = cur.fetchone()
+
+	return render_template("ventas.html",administrador=admin,name = nombre)
